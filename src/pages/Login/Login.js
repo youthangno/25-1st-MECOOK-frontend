65cff1416cbcb82ced5e7629f0af1e79e2cfcb58
goun <sgw7546@gmail.com>
2021-10-06 21:49:48 +0900

    Add:login 페이지 레이아웃 및 기능 구현

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,8 +1,76 @@
 import React from 'react';
-
+import './Login.scss';
+//import './variable.scss';
 class Login extends React.Component {
+  constructor() {
+    super();
+    this.state = { id: '', pw: '', isActive: false };
+  }
+  goTOMain = () => {
+    this.props.history.push('/main');
+    /* fetch('http://10.58.1.9:8000/users/signin/', {
+      method: 'POST',
+      body: JSON.stringify({
+        email: this.state.Id,
+        password: this.state.Pw,
+        //phone: '010326445555',
+        //name: 'anna',
+      }),
+    })
+      .then(response => response.json())
+      .then(result => console.log('결과: ', result));*/
+  };
+
+  handleIdInput = event => {
+    this.setState({ id: event.target.value });
+    console.log(this.state);
+  };
+  changeButton = () => {
+    const { id, pw } = this.state;
+    id.includes('@') && pw.length >= 8
+      ? this.setState({ isActive: true })
+      : this.setState({ isActive: false });
+  };
   render() {
-    return <h1>Login</h1>;
+    return (
+      <div className="outBox">
+        <button class="close">X</button>
+        <h1>LOGIN</h1>
+        <div class="infoContainer">
+          <input
+            Class="myInfo"
+            onchage={this.handleIdInput}
+            onKeyUp={this.changeButton}
+            type="text"
+            placeholder="아이디"
+          ></input>
+        </div>
+        <div className="numContainer">
+          <input
+            onchange={this.handleIdInput}
+            onKeyUp={this.changeButton}
+            class="secretNumber"
+            type="password"
+            placeholder="비밀번호"
+          ></input>
+        </div>
+        <span class="searchBox">
+          <a herf="">아이디 찾기</a>
+          <span class="slash">/</span>
+          <a herf=""> 비밀번호 찾기</a>
+        </span>
+        <span className="saveBtn">아이디 저장</span>
+
+        <button
+          className={`loginBtn ${this.state.isActive ? 'Active' : 'Disabled'}`}
+          onClick={this.goTOMain}
+        >
+          LOGIN
+        </button>
+
+        <button class="signInBtn"> SIGN IN </button>
+      </div>
+    );
   }
 }
 
