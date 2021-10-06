ff80790eafbb8a2e6c6dda202a0836af7a17d1ae
yonghyun <dydgus0421@gmail.com>
2021-10-06 21:49:56 +0900

    Modify : css 수정

--- src/pages/Main/Gourmet/Gourmet.js
+++ src/pages/Main/Gourmet/Gourmet.js
@@ -14,11 +14,11 @@ class Gourmet extends Component {
             <p>맛집콜라보</p>
             <img alt="matzip" src="/images/Main/matzip.jpg" />
           </div>
-          <div className="gList1">
+          <div className="gList2">
             <p>미쿡반점</p>
             <img alt="joongsik" src="/images/Main/chinese.jpg" />
           </div>
-          <div className="gList1">
+          <div className="gList3">
             <p>만두 연구소</p>
             <img alt="mandoo" src="/images/Main/dumpling.jpg" />
           </div>
