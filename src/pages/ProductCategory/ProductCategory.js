7ec5787596ec92447aa18dcf5c6fb6dbf7371dae
jeong-minji <alswl4152@naver.com>
2021-10-06 16:45:17 +0900

    Modify: Mock data로 Nav 메뉴 따라 카테고리 리스트 변경 기능 구현

--- src/pages/ProductCategory/ProductCategory.js
+++ src/pages/ProductCategory/ProductCategory.js
@@ -2,8 +2,10 @@ import React from 'react';
 import ProductCategoryList from './ProductCategoryList/ProductCategoryList';
 import ProductList from '../ProductList/ProductList';
 import './ProductCategory.scss';
+import CATEGORY_DATA from './productCategoryData';
 
-const categories = ['KOREAN', 'CHINESE', 'JAPANESE', 'ITALIAN'];
+// Header와 연결시 헤더에서 누른 카테고리값 props로 받아올 부분
+const category = 'DINING';
 
 class ProductCategory extends React.Component {
   state = {
@@ -32,7 +34,7 @@ class ProductCategory extends React.Component {
             <span>DINING</span>
           </p>
           <ul className="categoryListContainer">
-            {categories.map((category, index) => {
+            {CATEGORY_DATA[category].map((category, index) => {
               return (
                 <ProductCategoryList
                   key={index}
---
+++ src/pages/ProductCategory/productCategoryData.js
@@ -0,0 +1,7 @@
+const CATEGORY_DATA = {
+  DINING: ['KOREAN', 'CHINESE', 'JAPANESE', 'ITALIAN'],
+  CAFE: ['SWEET', 'BAKERY', 'BAVERAGE'],
+  CUPBOARD: ['CEREAL', 'SPICE', 'SNACK'],
+};
+
+export default CATEGORY_DATA;
