2c2e15c062ad1f8dd0a7b0f29ddaa0cc4342437e
yonghyun <dydgus0421@gmail.com>
2021-10-08 18:58:36 +0900

    Add : Review Component 분리

--- src/Routes.js
+++ src/Routes.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
-import Main from './pages/Main/Main';
-import Login from './pages/Login/Login';
-import MinMenu from './pages/Main/MinMenu/MinMenu';
 import Review from './pages/Review/Review';
 
 class Routes extends React.Component {
@@ -11,9 +8,6 @@ class Routes extends React.Component {
     return (
       <Router>
         <Switch>
-          <Route exact path="/" component={Main} />
-          <Route exact path="/login" component={Login} />
-          <Route exact path="/minmenu" component={MinMenu} />
           <Route exact path="/review" component={Review} />
         </Switch>
       </Router>
