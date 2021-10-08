022f3827d694ca6c361b503c21ea05e80ee6b82f
goun <sgw7546@gmail.com>
2021-10-08 10:55:30 +0900

    add: 회원가입 페이지 레이아웃 구현

--- src/Routes.js
+++ src/Routes.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import Main from './pages/Main/Main';
 import Login from './pages/Login/Login';
-
+import Signin from './pages/Signin/Signin';
 class Routes extends React.Component {
   render() {
     return (
@@ -11,6 +11,7 @@ class Routes extends React.Component {
         <Switch>
           <Route exact path="/" component={Main} />
           <Route exact path="/login" component={Login} />
+          <Route exact path="/signin" component={Signin} />
         </Switch>
       </Router>
     );
---
+++ src/pages/Signin/signin.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import './Signin.scss';
+//import './variable.scss';
+class Signin extends React.Component {
+  constructor() {
+    super();
+    this.state = { name: '', id: '', pw: '', email: '' };
+  }
+
+  render() {
+    return (
+      <div className="outContainer">
+        <button class="closeBtn">X</button>
+        <h1>SIGNIN</h1>
+        <div className="nameBox">
+          <input className="nameInfo" placeholder="이름" />
+        </div>
+        <div class="idBox">
+          <input Class="idInfo" placeholder="아이디" />
+        </div>
+        <div className="numBox">
+          <input class="secretNum" type="password" placeholder="비밀번호" />
+        </div>
+        <div className="emailBox">
+          <input className="emailInfo" placeholder="이메일" />
+        </div>
+        <input type="checkbox" className="agreeCheck" /> 동의
+        <button class="join"> 가입하기 </button>
+      </div>
+    );
+  }
+}
+
+export default Signin;
