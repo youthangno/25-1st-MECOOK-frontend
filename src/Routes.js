c187bde306af18b3f49bebc409e5e182ae2514be
youthangno <youthangno@gmail.com>
2021-10-08 21:59:00 +0900

    Add: 검색창 레이아웃

--- src/Routes.js
+++ src/Routes.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Main from './pages/Main/Main';
 import Login from './pages/Login/Login';
 import ProductDetail from './pages/ProductDetail/ProductDetail';
+import Search from './pages/Search/Search';
 
 class Routes extends React.Component {
   render() {
@@ -13,6 +14,7 @@ class Routes extends React.Component {
           <Route exact path="/" component={Main} />
           <Route exact path="/login" component={Login} />
           <Route exact path="/productdetail" component={ProductDetail} />
+          <Route exact path="/search" component={Search} />
         </Switch>
       </Router>
     );
---
+++ src/pages/Search/Search.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import './Search.scss';
+class Search extends Component {
+  render() {
+    return (
+      <>
+        <div className="searchArea">
+          <div className="searchBox">
+            <form>
+              <input className="searchInput" placeholder="SEARCH" />
+              <button>
+                <i className="fas fa-ban"></i>
+              </button>
+              <button>
+                <i className="fas fa-search"></i>
+              </button>
+            </form>
+            <span>당신의 맛있는 하루를 위한</span>
+            <ul className="hashTag">
+              <li># 샐러드</li>
+              <li># 만두연구소</li>
+              <li># 집밥</li>
+              <li># 맛집</li>
+              <li># 간식</li>
+              <li># 보양식</li>
+              <li># 홈파티</li>
+              <li># 곤드레</li>
+            </ul>
+          </div>
+        </div>
+      </>
+    );
+  }
+}
+
+export default Search;
