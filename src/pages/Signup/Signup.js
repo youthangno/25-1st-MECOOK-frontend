b5915dea593f0a4605f3dee38ba877bb8ac4b614
goun <sgw7546@gmail.com>
2021-10-09 11:08:47 +0900

    modify : 파일 경로 수정

---
+++ src/pages/Signup/Signup.js
@@ -0,0 +1,96 @@
+import React from 'react';
+import './Signup.scss';
+//import './variable.scss';
+class Signup extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      name: '',
+      user_id: '',
+      password: '',
+      email: '',
+      pwCheck: '',
+      usableId: false,
+    };
+  }
+
+  goToMain = () => {
+    /*fetch('http://10.58.2.115:8000/user/signup', {
+      method: 'POST',
+      body: JSON.stringify({
+        account: this.state.id,
+        password: this.state.pw,
+        name: this.state.name,
+        email: this.state.email,
+      }),
+    })
+      .then(response => response.json())
+      .then(result => console.log('결과: ', result));
+    this.props.history.push('/');*/
+  };
+
+  handleInput = event => {
+    this.setState({ [event.target.name]: event.target.value });
+  };
+
+  render() {
+    return (
+      <div className="outContainer">
+        <button class="closeBtn">X</button>
+        <h1>SIGNIN</h1>
+        <div className="nameBox">
+          <input
+            className="nameInfo"
+            placeholder="이름"
+            onChange={this.handleInput}
+            name="name"
+          />
+        </div>
+        <div className="idBox">
+          <input
+            className="idInfo"
+            placeholder="아이디"
+            onChange={this.handleInput}
+            name="id"
+          />
+        </div>
+        <button className="sameCheck" onclick={this.checkId}>
+          중복확인
+        </button>
+        <div className="numBox">
+          <input
+            className="secretNum"
+            type="password"
+            placeholder="비밀번호"
+            onChange={this.handleInput}
+            name="pw"
+          />
+        </div>
+        <div className="checkPw">
+          <input
+            className="checkNumber"
+            type="password"
+            placeholder="비밀번호 확인"
+            name="pwcheck"
+          />
+        </div>
+        <div className="emailBox">
+          <inputgit
+            className="emailInfo"
+            placeholder="이메일"
+            onChange={this.handleInput}
+            name="email"
+          />
+        </div>
+        <input type="checkbox" className="agreeCheck" />
+        <p>회원약관에 동의하시면 체크해주세요.</p>
+
+        <button className="join" onClick={this.goToMain}>
+          가입하기
+        </button>
+      </div>
+    );
+  }
+}
+
+export default Signup;
