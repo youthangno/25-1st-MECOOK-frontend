ec2ef0a7bbbb07abfd7d59f9d008c6f0d25d43f0
youthangno <youthangno@gmail.com>
2021-10-11 17:05:52 +0900

    modify:장바구니로 보낼 데이터 준비

--- src/pages/ProductDetail/ProductInfo/Btn/AddCartBtn/AddCartBtn.js
+++ src/pages/ProductDetail/ProductInfo/Btn/AddCartBtn/AddCartBtn.js
@@ -2,8 +2,40 @@ import React from 'react';
 import './AddCartBtn.scss';
 
 class AddCartBtn extends React.Component {
+  constructor() {
+    super();
+  }
+
+  clickCartBtn = () => {
+    const TOKEN = localStorage.getItem('token');
+    if (TOKEN) {
+      fetch('', {
+        method: 'POST',
+        headers: {
+          Authorization: TOKEN,
+        },
+        body: JSON.stringify({
+          ProductId: 1,
+          ProductCount: 5,
+        }),
+      })
+        .then(res => res.json())
+        .then(data => {
+          this.setState({
+            cartList: data,
+          });
+        });
+    } else {
+      alert('로그인 한 사용자만 이용 가능합니다.');
+    }
+  };
+
   render() {
-    return <button className="addCartBtn">ADD TO MY MENU</button>;
+    return (
+      <button onClick={this.clickCartBtn} className="addCartBtn">
+        ADD TO MY MENU
+      </button>
+    );
   }
 }
 
