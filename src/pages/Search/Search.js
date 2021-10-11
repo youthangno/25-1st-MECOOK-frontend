fa5c102d56aeb8ca6532512476638a4f0f22403b
youthangno <youthangno@gmail.com>
2021-10-11 16:16:48 +0900

    ADD:Search연걸

--- src/pages/Search/Search.js
+++ src/pages/Search/Search.js
@@ -1,7 +1,33 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import './Search.scss';
 class Search extends Component {
+  constructor() {
+    super();
+    this.state = {
+      hashtagList: [],
+    };
+  }
+  componentDidMount() {
+    fetch(
+      'https://f960-211-106-114-186.ngrok.io/product/menu/category/1/detail',
+      {
+        method: 'GET',
+      }
+    )
+      .then(res => res.json())
+      .then(data => {
+        this.setState({
+          hashtagList: data.result[0].hashtag,
+        });
+      });
+  }
   render() {
+    const hashtagList = this.state.hashtagList;
+    hashtagList.map(list => {
+      return list;
+    });
+
     return (
       <>
         <div className="searchArea">
@@ -17,6 +43,7 @@ class Search extends Component {
             </form>
             <span>당신의 맛있는 하루를 위한</span>
             <ul>
+              {<li><Link to=''>{hashtagList && {`#${hashtagList}`}}<Link/></li>}
               {/* <li># 샐러드</li>
               <li># 만두연구소</li>
               <li># 집밥</li>
