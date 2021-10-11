f84cc1f6aa4a41faaa6206739f966c0493ea9598
yonghyun <dydgus0421@gmail.com>
2021-10-11 11:28:02 +0900

    Add : Gourmet 컴포넌트 부분수정 및 기능 추가

--- src/pages/Main/MainImg/MainImg.js
+++ src/pages/Main/MainImg/MainImg.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import '../MainImg/MainImg.scss';
 
 class MainImg extends Component {
@@ -6,9 +7,11 @@ class MainImg extends Component {
     return (
       <section className="mainImg">
         <img alt="hamburger" src="/images/Main/hamburger.jpg" />
-        <p>
-          PRINCESSMAKER&nbsp;<i className="far fa-play-circle"></i>
-        </p>
+        <a href="https://namu.wiki/w/%ED%94%84%EB%A6%B0%EC%84%B8%EC%8A%A4%20%EB%A9%94%EC%9D%B4%EC%BB%A4">
+          <p>
+            PRINCESSMAKER&nbsp;<i className="far fa-play-circle"></i>
+          </p>
+        </a>
       </section>
     );
   }
