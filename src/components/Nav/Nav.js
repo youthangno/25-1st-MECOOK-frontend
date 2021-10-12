87bc3c5922d505801d0fc8fcd807edc89e48f01a
yonghyun <dydgus0421@gmail.com>
2021-10-12 18:41:28 +0900

    Add : Nav의 새로운 시작

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,8 +1,108 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import './Nav.scss';
 
-class Nav extends React.Component {
+class Nav extends Component {
+  constructor() {
+    super();
+    this.state = {
+      scrollTop: 0,
+      isLogin: false,
+      category: ['DINING', 'CAFE', 'CUPBOARD'],
+    };
+  }
+
+  handleScroll = e => {
+    const scrollTop = ('scroll', e.srcElement.scrollingElement.scrollTop);
+    this.setState({
+      scrollTop,
+    });
+  };
+
+  componentWillUnMount = () => {
+    window.removeEventListener('scroll', this.handleScroll);
+  };
+
+  componentDidMount = () => {
+    window.addEventListener('scroll', this.handleScroll);
+
+    if (!localStorage.getItem('userToken')) {
+      this.setState({
+        isLogin: false,
+      });
+    } else {
+      this.setState({
+        isLogin: true,
+      });
+    }
+  };
+
+  handleLog = () => {
+    if (localStorage.getItem('userToken') && this.state.isLogin) {
+      this.setState({
+        isLogin: false,
+      });
+      alert('로그아웃 되었습니다.');
+      localStorage.removeItem('userToken');
+    }
+  };
+
+  test = () => {
+    if (!localStorage.getItem('userToken')) {
+      localStorage.setItem('userToken', 'asd');
+      this.setState({
+        isLogin: true,
+      });
+      alert('로그인 되었습니다.');
+    }
+  };
+
   render() {
-    return null;
+    const { scrollTop } = this.state;
+    console.log(this.state);
+    console.log(localStorage);
+    return (
+      <>
+        <nav
+          className={!scrollTop ? 'original' : 'original change'}
+          onScroll={this.handleScroll}
+        >
+          <div className="threeLine">
+            <Link to="/minmenu">
+              <i className="fas fa-bars"></i>
+            </Link>
+            MECOOK
+          </div>
+          <div className="logo">
+            <Link to="/">MECOOK</Link>
+          </div>
+          <div className="menu">
+            {this.state.category.map(a => (
+              <Link
+                to={{ pathname: '/page', state: { category: a } }}
+                className="menuBtn"
+              >
+                {a}
+              </Link>
+            ))}
+            <Link to="/best" className="menuBtn">
+              BEST&#8314;
+            </Link>
+            <Link to="/search" className="menuBtn">
+              SEARCH
+            </Link>
+          </div>
+          <div className="login">
+            <button className="loginBtn" onClick={this.handleLog}>
+              {this.state.isLogin ? 'L O G O U T' : 'L O G I N'}
+            </button>
+            <Link to="/search">
+              <i className="fas fa-search"></i>
+            </Link>
+          </div>
+        </nav>
+      </>
+    );
   }
 }
 
