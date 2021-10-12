de9d85849c160e1e8335375c79951b1c5c76933b
youthangno <youthangno@gmail.com>
2021-10-12 18:32:21 +0900

    ADD: Search브랜치   Osearch새 브랜치로 이동

---
+++ src/pages/Search/Search.js
@@ -0,0 +1,57 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import HashTag from './HashTag/HashTag';
+import './Search.scss';
+class Search extends Component {
+  constructor() {
+    super();
+    this.state = {
+      inputData: '',
+    };
+  }
+
+  handleSubmit = e => {
+    e.preventDefault();
+  };
+
+  handleInput = e => {
+    this.setState({
+      inputData: e.target.value,
+    });
+  };
+
+  handleReset = () => {
+    this.setState({
+      inputData: '',
+    });
+  };
+
+  render() {
+    return (
+      <>
+        <div className="searchArea">
+          <div className="searchBox">
+            <form onSubmit={this.handleSubmit}>
+              <input
+                type="text"
+                className="searchInput"
+                placeholder="SEARCH"
+                onChange={this.handleInput}
+                value={this.state.inputData}
+              />
+              <button>
+                <i className="fas fa-ban" onClick={this.handleReset}></i>
+              </button>
+              <button>
+                <i className="fas fa-search" onClick={'#'}></i>
+              </button>
+            </form>
+            <HashTag />
+          </div>
+        </div>
+      </>
+    );
+  }
+}
+
+export default Search;
