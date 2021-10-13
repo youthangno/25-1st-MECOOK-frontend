10788b467c57cbcf68912fa18404959b8b16cdbc
yonghyun <dydgus0421@gmail.com>
2021-10-14 01:21:59 +0900

    소문자 대문자

--- src/pages/ProductCategory/productCategoryData.js
+++
@@ -1,7 +0,0 @@
-const CATEGORY_DATA = {
-  dining: ['KOREAN', 'CHINESE', 'JAPANESE', 'ITALIAN'],
-  cafe: ['SWEET', 'BAKERY', 'BAVERAGE'],
-  cupboard: ['CEREAL', 'SPICE', 'SNACK'],
-};
-
-export default CATEGORY_DATA;
