0b64aaa31af682447f3e818d0ed984288140eb8d
yonghyun <dydgus0421@gmail.com>
2021-10-13 17:26:18 +0900

    Modify : footer merge 및 Routes 수정

--- src/Routes.js
+++ src/Routes.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
+import Nav from './components/Nav/Nav';
 import Main from './pages/Main/Main';
 import Login from './pages/Login/Login';
+import Footer from './components/Footer/Footer';
 
 class Routes extends React.Component {
   render() {
     return (
       <Router>
+        <Nav />
         <Switch>
           <Route exact path="/" component={Main} />
           <Route exact path="/login" component={Login} />
         </Switch>
+        <Footer />
       </Router>
     );
   }
