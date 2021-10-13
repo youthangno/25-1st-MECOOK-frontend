13aad23e84513e88953ff1c516c53fb3fbc2fd97
yonghyun <dydgus0421@gmail.com>
2021-10-13 15:34:11 +0900

    Modify : Routes 경로 수정

--- src/Routes.js
+++ src/Routes.js
@@ -3,14 +3,13 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import Main from './pages/Main/Main';
 import Login from './pages/Login/Login';
-import Nav from './components/Nav/Nav';
 
 class Routes extends React.Component {
   render() {
     return (
       <Router>
         <Switch>
-          <Route exact path="/" component={Nav} />
+          <Route exact path="/" component={Main} />
           <Route exact path="/login" component={Login} />
         </Switch>
       </Router>
