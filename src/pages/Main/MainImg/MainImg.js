ad2ca4c3bda55509df5c28b69e2d81add72d82e1
yonghyun <dydgus0421@gmail.com>
2021-10-13 16:38:19 +0900

    Modify : 약간 수정

--- src/pages/Main/MainImg/MainImg.js
+++ src/pages/Main/MainImg/MainImg.js
@@ -3,9 +3,15 @@ import '../MainImg/MainImg.scss';
 
 class MainImg extends Component {
   render() {
+    this.carousel();
+
     return (
       <section className="mainImg">
-        <img alt="hamburger" src="/images/Main/hamburger.jpg" />
+        <img
+          className="mySlides"
+          alt="hamburger"
+          src="/images/Main/hamburger.jpg"
+        />
         <a href="https://namu.wiki/w/%ED%94%84%EB%A6%B0%EC%84%B8%EC%8A%A4%20%EB%A9%94%EC%9D%B4%EC%BB%A4">
           <p>
             PRINCESSMAKER&nbsp;<i className="far fa-play-circle"></i>
