38a8bd8e5bf514f3e9ea02e0b3181dce484a616f
yonghyun <dydgus0421@gmail.com>
2021-10-14 18:27:35 +0900

    css 수정

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import Nav from '../../components/Nav/Nav';
 import MainImg from './MainImg/MainImg';
 import Best from './Best/Best';
 import Brand from './Brand/Brand';
